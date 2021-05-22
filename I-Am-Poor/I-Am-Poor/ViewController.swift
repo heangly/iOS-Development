85f390e04176eda155106569380d1dbab09b7722
Heang Ly <heanglypa@gmail.com>
2021-05-22 14:15:26 -0400

    I am poor finished

---
+++ I-Am-Poor/I-Am-Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-Am-Poor
+//
+//  Created by Heang Ly on 5/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
