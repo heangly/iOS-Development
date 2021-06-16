973f4eb167143ac48d436712ccf436002efb4874
Heang Ly <heanglypa@gmail.com>
2021-06-15 23:52:24 -0400

    add fetching method

--- Clima-iOS13/Clima/Model/WeatherManager.swift
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -8,13 +8,46 @@
 
 import Foundation
 
+extension Data {
+    var prettyPrintedJSONString: NSString? { /// NSString gives us a nice sanitized debugDescription
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
+    }
+}
+
 
 struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=97acd4846c591df762b1d82f3e1b5323&units=metric"
 
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            task.resume()
+        }
+
+    }
+    
+
+
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil{
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString!.data(using: .utf8)?.prettyPrintedJSONString!)
+        }
     }
 }
 
