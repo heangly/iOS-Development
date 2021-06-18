eaad91b5bb6bbd1d9580bd473acb2838fe115028
Heang Ly <heanglypa@gmail.com>
2021-06-18 16:51:53 -0400

    part 1

--- Guess-The-Flag/Guess-The-Flag/ViewController.swift
+++ Guess-The-Flag/Guess-The-Flag/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+
+    var countries = [String]()
+    var score = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        countries.append(contentsOf: ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"])
+        askQuestion()
     }
 
+    func askQuestion() {
+        button1.setImage(UIImage(named: getRandomImageName()), for: .normal)
+        button1.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        
+        button2.setImage(UIImage(named: getRandomImageName()), for: .normal)
+        button2.layer.borderWidth = 1
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        
+        button3.setImage(UIImage(named: getRandomImageName()), for: .normal)
+        button3.layer.borderWidth = 1
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+      
+    }
+
+    func getRandomImageName() -> String {
+        let idx = Int.random(in: 0..<countries.count)
+        return countries[idx]
+    }
 
 }
 
