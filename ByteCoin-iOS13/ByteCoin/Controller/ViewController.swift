46bb022aabf122d40fc75c7c586a41370d96b7dd
Heang Ly <heanglypa@gmail.com>
2021-06-18 14:06:29 -0400

    complete

---
+++ ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var coinManager = CoinManager()
+
+    @IBOutlet var bitcoinLabel: UILabel!
+    @IBOutlet var currencyLabel: UILabel!
+    @IBOutlet var currencyPicker: UIPickerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        coinManager.delegate = self
+    }
+}
+
+//MARK: - CoinMangerDelegate
+extension ViewController: CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+//MARK: - UIPickerView DataSource & Delegate
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        coinManager.getCoinPrice(for: coinManager.currencyArray[row])
+    }
+}
---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Heang Ly on 6/18/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable{
+    let rate: Double
+}
