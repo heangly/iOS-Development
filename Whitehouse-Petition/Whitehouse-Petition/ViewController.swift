5986b5f5308a096cd0e9c128ab1647a5a4d7c5c5
Heang Ly <heanglypa@gmail.com>
2021-06-30 14:35:13 -0400

    petition

--- Whitehouse-Petition/Whitehouse-Petition/ViewController.swift
+++ Whitehouse-Petition/Whitehouse-Petition/ViewController.swift
@@ -8,27 +8,42 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    
+
     var petitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let urlString =  "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        showError()
     }
-    
-    func parse(json: Data){
+
+    func parse(json: Data) {
         let decoder = JSONDecoder()
-        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
             tableView.reloadData()
         }
     }
-    
+
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "ok", style: .default))
+        present(ac, animated: true)
+    }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
@@ -41,7 +56,9 @@ class ViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 }
