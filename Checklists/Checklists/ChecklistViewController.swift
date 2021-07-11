1ad51b5432de2164eeca4e4e71684962a9a90dd1
Heang Ly <heanglypa@gmail.com>
2021-07-11 17:15:21 -0400

    set up

---
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Heang Ly on 7/11/21.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    //MARK: - Table View Data Source
+    
+
+}
+
