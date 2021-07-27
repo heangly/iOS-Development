bce0ae7c6d90bfa67e983c7bdeb56a4abdbbc209
Heang Ly <heanglypa@gmail.com>
2021-07-27 14:50:01 -0400

    finish authentication screen

---
+++ InstagramFirestore/InstagramFirestore/Controller/Authentication/LoginController.swift
@@ -0,0 +1,115 @@
+//
+//  LoginController.swift
+//  InstagramFirestore
+//
+//  Created by Heang Ly on 7/27/21.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    //MARK: - Properties
+    private var viewModel = LoginViewModel()
+    
+    private let iconImage: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+
+    private let emailTextField: UITextField = {
+        let tf = CustomTextField(placeholder: "Email")
+        tf.keyboardType = .emailAddress
+        return tf
+    }()
+
+    private let passwordTextField: UITextField = {
+        let tf = CustomTextField(placeholder: "Password")
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log in", for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1).withAlphaComponent(0.5)
+        button.setTitleColor(UIColor(white: 1, alpha: 0.67), for: .normal)
+        button.isEnabled = false
+        button.layer.cornerRadius = 5
+        button.setHeight(50)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+
+    private let forgetPassButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "Forget your password?", secondPart: "Get help singing in.")
+        return button
+    }()
+
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.attributedTitle(firstPart: "Dont' have an account?", secondPart: "Sign Up")
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
+
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        configureNotificationObservers()
+    }
+    
+    //MARK: - Actions
+    @objc func handleShowSignUp(){
+        let controller = RegistrationController()
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    @objc func textDidChange(sender: UITextField){
+        if sender == emailTextField {
+            viewModel.email = sender.text
+        }else{
+            viewModel.password = sender.text
+        }
+        updateForm()
+    }
+
+    //MARK: - Helpers
+    func configureUI() {
+        configureGradientLayer()
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.setDimensions(height: 80, width: 120)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+
+        let stack = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, loginButton, forgetPassButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
+    }
+
+    func configureNotificationObservers(){
+        emailTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+    }
+}
+
+//MARK: - Update Form
+extension LoginController: FormViewModel {
+    func updateForm() {
+        loginButton.backgroundColor = viewModel.buttonBackgroundColor
+        loginButton.setTitleColor(viewModel.buttonTitleColor, for: .normal)
+        loginButton.isEnabled = viewModel.formIsValid
+    }
+}
--- InstagramFirestore/InstagramFirestore/SceneDelegate.swift
+++ InstagramFirestore/InstagramFirestore/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = MainTabController()
+//        window?.rootViewController = MainTabController()
+        window?.rootViewController = UINavigationController(rootViewController: LoginController())
         window?.makeKeyAndVisible()
     }
 
