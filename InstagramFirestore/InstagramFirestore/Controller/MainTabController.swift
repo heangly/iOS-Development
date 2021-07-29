2db684f955b361e0db74088b177013d49ae32330
Heang Ly <heanglypa@gmail.com>
2021-07-29 16:25:32 -0400

    add profille screen

--- InstagramFirestore/InstagramFirestore/Controller/MainTabController.swift
+++ InstagramFirestore/InstagramFirestore/Controller/MainTabController.swift
@@ -53,8 +53,8 @@ class MainTabController: UITabBarController {
         let imageSelector = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorController())
 
         let notification = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationController())
-
-        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController())
+        
+        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController(collectionViewLayout: layout))
 
 //        put all instances of each tab to tab bar
         viewControllers = [feed, search, imageSelector, notification, profile]
---
+++ InstagramFirestore/InstagramFirestore/View/Profile/ProfileCell.swift
@@ -0,0 +1,35 @@
+//
+//  ProfileCell.swift
+//  InstagramFirestore
+//
+//  Created by Heang Ly on 7/29/21.
+//
+
+import UIKit
+
+class ProfileCell: UICollectionViewCell {
+    //MARK: - Properties
+    
+    
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .lightGray
+        addSubview(postImageView)
+        postImageView.fillSuperview()
+    }
+    
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
