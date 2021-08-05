3a2f586998214f6680a23d1bb7a8508a4deaa514
Heang Ly <heanglypa@gmail.com>
2021-08-05 18:30:07 -0400

    auth

--- SpotifyClone/SpotifyClone/Controllers/Other/AuthViewController.swift
+++ SpotifyClone/SpotifyClone/Controllers/Other/AuthViewController.swift
@@ -6,24 +6,46 @@
 //
 
 import UIKit
+import WebKit
 
-class AuthViewController: UIViewController {
+class AuthViewController: UIViewController, WKNavigationDelegate {
+    private let webView: WKWebView = {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+
+        let config = WKWebViewConfiguration()
+        config.defaultWebpagePreferences = prefs
+
+        let webView = WKWebView(frame: .zero, configuration: config)
+        return webView
+    }()
+
+    public var completionHandler: ((Bool) -> Void)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Sign In"
+        view.backgroundColor = .systemBackground
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+        guard let url = AuthManager.shared.signInURL else { return }
+        webView.load(URLRequest(url: url))
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
+
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        guard let url = webView.url else { return }
+
+//        Exchange The code for access token
+        let component = URLComponents(string: url.absoluteString)
+        guard let code = component?.queryItems?.first(where: { $0.name == "code" })?.value else { return }
+        print(code)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- SpotifyClone/SpotifyClone/Managers/AuthManger.swift
+++ SpotifyClone/SpotifyClone/Managers/AuthManger.swift
@@ -10,7 +10,19 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
 
+    struct Constants {
+        static let clientID = "d83b9980ddca450c976fb6e4be9969f6"
+    }
+
     private init() { }
+
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
 
     var isSignedIn: Bool {
         return false
@@ -30,6 +42,18 @@ final class AuthManager {
 
     private var shouldRefreshToken: Bool {
         return false
+    }
+
+    public func exchangeCodeForToken(code: String, completion: @escaping (Bool) -> Void) {
+
+    }
+
+    public func refreshAccessToken() {
+
+    }
+
+    private func cacheToken() {
+
     }
 
 }
