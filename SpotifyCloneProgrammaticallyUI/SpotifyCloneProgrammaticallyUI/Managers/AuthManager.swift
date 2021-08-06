bc903c31ee24ab1b4f7f9561b06a1d1fcc495c0d
Heang Ly <heanglypa@gmail.com>
2021-08-06 09:09:59 -0400

    part 2

--- SpotifyCloneProgrammaticallyUI/SpotifyCloneProgrammaticallyUI/Managers/AuthManager.swift
+++ SpotifyCloneProgrammaticallyUI/SpotifyCloneProgrammaticallyUI/Managers/AuthManager.swift
@@ -9,26 +9,50 @@ import Foundation
 
 final class AuthManger {
     static let shared = AuthManger()
-    
-    private init(){}
-    
+
+    struct Constants {
+        static let clientID = "d83b9980ddca450c976fb6e4be9969f6"
+    }
+
+    private init() { }
+
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
+
     var inSignedIn: Bool {
         return false
     }
-    
+
     private var accessToken: String? {
         return nil
     }
-    
+
     private var refreshToken: String? {
         return nil
     }
-    
+
     private var expirationDate: Date? {
         return nil
     }
-    
+
     private var shouldRefreshToken: Bool {
         return false
+    }
+
+    public func exchangeCodeForToken(code: String, completion: @escaping (Bool) -> Void) {
+
+    }
+
+    public func refreshAccessToken() {
+
+    }
+
+    private func cacheToken() {
+
     }
 }
