085b6d17fb53b9e83c3ee814b2e920c9e0ea18c7
Heang Ly <heanglypa@gmail.com>
2021-08-09 10:29:23 -0400

    login screen

---
+++ InstagramFirestore/InstagramFirestore/Controllers/Authentication/RegisterationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RegisterationViewController.swift
+//  InstagramFirestore
+//
+//  Created by Heang Ly on 8/9/21.
+//
+
+import UIKit
+
+class RegisterationViewController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    func configureUI(){
+        view.backgroundColor = .systemPink
+    }
+}
