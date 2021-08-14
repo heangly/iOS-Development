fb9ec31033abe69ec15eaec9b71940165deee858
Heang Ly <heanglypa@gmail.com>
2021-08-14 17:59:27 -0400

    finish profile tab ui

--- InstagramFirestoreTut/InstagramFirestoreTut/Views/Profile/ProfileCell.swift
+++ InstagramFirestoreTut/InstagramFirestoreTut/Views/Profile/ProfileCell.swift
@@ -7,12 +7,24 @@
 
 import UIKit
 
-class ProfileCell: UICollectionViewCell{
+class ProfileCell: UICollectionViewCell {
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .lightGray
+        backgroundColor = .systemBackground
+
+        addSubview(imageView)
+        imageView.fillSuperview()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- InstagramFirestoreTut/InstagramFirestoreTut/Views/Profile/ProfileHeader.swift
+++ InstagramFirestoreTut/InstagramFirestoreTut/Views/Profile/ProfileHeader.swift
@@ -8,12 +8,152 @@
 import UIKit
 
 class ProfileHeader: UICollectionReusableView {
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        iv.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        iv.layer.cornerRadius = 80 / 2
+        return iv
+    }()
+
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Eddie Brock"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+
+    private let editProfileFollowButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Edit Profile", for: .normal)
+        btn.layer.cornerRadius = 3
+        btn.layer.borderColor = UIColor.lightGray.cgColor
+        btn.layer.borderWidth = 0.5
+        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        btn.setTitleColor(.black, for: .normal)
+        btn.addTarget(self, action: #selector(handleEditProfileFollowTapped), for: .touchUpInside)
+        return btn
+    }()
+
+    private lazy var postsLabel: UILabel = {
+        let label = UILabel()
+        label.attributedText = attributedStatText(value: 5, label: "posts")
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var followersLabel: UILabel = {
+        let label = UILabel()
+        label.attributedText = attributedStatText(value: 2, label: "followers")
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+
+    private lazy var followingsLabel: UILabel = {
+        let label = UILabel()
+        label.attributedText = attributedStatText(value: 1, label: "following")
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+
+    private let gridButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(#imageLiteral(resourceName: "grid"), for: .normal)
+        return btn
+    }()
+
+    private let listButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(#imageLiteral(resourceName: "list"), for: .normal)
+        btn.tintColor = UIColor(white: 0, alpha: 0.2)
+        return btn
+    }()
+
+    private let bookmarkButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(#imageLiteral(resourceName: "ribbon"), for: .normal)
+        btn.tintColor = UIColor(white: 0, alpha: 0.2)
+        return btn
+    }()
+
+
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemPink
+        backgroundColor = .systemBackground
+
+        addSubview(profileImageView)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
+        profileImageView.topAnchor.constraint(equalTo: topAnchor, constant: 12).isActive = true
+
+        addSubview(nameLabel)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 12).isActive = true
+        nameLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 12).isActive = true
+
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.translatesAutoresizingMaskIntoConstraints = false
+        editProfileFollowButton.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 12).isActive = true
+        editProfileFollowButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 24).isActive = true
+        editProfileFollowButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -24).isActive = true
+
+        let stack = UIStackView(arrangedSubviews: [postsLabel, followersLabel, followingsLabel])
+        addSubview(stack)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.centerY(inView: profileImageView)
+        stack.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 12).isActive = true
+        stack.rightAnchor.constraint(equalTo: rightAnchor, constant: -12).isActive = true
+        stack.distribution = .fillEqually
+
+        let buttonStacks = UIStackView(arrangedSubviews: [gridButton, listButton, bookmarkButton])
+        addSubview(buttonStacks)
+        buttonStacks.translatesAutoresizingMaskIntoConstraints = false
+        buttonStacks.distribution = .fillEqually
+        buttonStacks.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        buttonStacks.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        buttonStacks.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        buttonStacks.heightAnchor.constraint(equalToConstant: 50).isActive = true
+ 
+        let topDivider = UIView()
+        topDivider.backgroundColor = .lightGray
+        addSubview(topDivider)
+        topDivider.translatesAutoresizingMaskIntoConstraints = false
+        topDivider.topAnchor.constraint(equalTo: buttonStacks.topAnchor).isActive = true
+        topDivider.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        topDivider.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        topDivider.heightAnchor.constraint(equalToConstant: 0.5).isActive = true
+        
+        let bottomDivider = UIView()
+        bottomDivider.backgroundColor = .lightGray
+        addSubview(bottomDivider)
+        bottomDivider.translatesAutoresizingMaskIntoConstraints = false
+        bottomDivider.topAnchor.constraint(equalTo: buttonStacks.bottomAnchor).isActive = true
+        bottomDivider.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        bottomDivider.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    //MARK: - Actions
+    @objc func handleEditProfileFollowTapped() {
+        print("DEBUG: handleEditProfileFollowTapped")
+    }
+
+    //MARK: - Helpers
+    func attributedStatText(value: Int, label: String) -> NSAttributedString {
+        let attributedText = NSMutableAttributedString(string: "\(value)\n", attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: label, attributes: [.font: UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.lightGray]))
+        return attributedText
+    }
 }
+
