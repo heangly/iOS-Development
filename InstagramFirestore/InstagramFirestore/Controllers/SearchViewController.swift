5d2fd1563d062b08fb96df039c3881115ea8851d
Heang Ly <heanglypa@gmail.com>
2021-08-17 07:47:25 -0400

    search UI

--- InstagramFirestore/InstagramFirestore/Controllers/SearchViewController.swift
+++ InstagramFirestore/InstagramFirestore/Controllers/SearchViewController.swift
@@ -7,9 +7,30 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+private let cellReuseIdentifier = "UserCell"
+
+class SearchViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        configureUI()
+    }
+    
+    //MARK: - Helpers
+    func configureUI(){
+        tableView.register(UserCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        tableView.rowHeight = 64
+//        tableView.separatorStyle = .none
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension SearchViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath) as! UserCell
+        return cell
     }
 }
---
+++ InstagramFirestore/InstagramFirestore/Views/UserCell.swift
@@ -0,0 +1,69 @@
+//
+//  UserCell.swift
+//  InstagramFirestore
+//
+//  Created by Heang Ly on 8/17/21.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    //MARK: - Properties
+    private let profileImageView: UIImageView = {
+        let pv = UIImageView()
+        pv.image = #imageLiteral(resourceName: "venom-7")
+        pv.contentMode = .scaleAspectFill
+        pv.clipsToBounds = true
+        pv.backgroundColor = .lightGray
+        return pv
+    }()
+
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Venom"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Eddie Brock"
+        label.textColor = .lightGray
+        return label
+    }()
+
+    //MARK: - Lifecycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+
+    //MARK: - Helpers
+    func configureUI() {
+        addSubview(profileImageView)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        profileImageView.widthAnchor.constraint(equalToConstant: 48).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        profileImageView.layer.cornerRadius = 48 / 2
+        profileImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 12).isActive = true
+        profileImageView.centerY(inView: self)
+
+        
+        let usernameStack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        addSubview(usernameStack)
+        usernameStack.axis = .vertical
+        usernameStack.spacing = 4
+        usernameStack.translatesAutoresizingMaskIntoConstraints = false
+        usernameStack.centerY(inView: profileImageView)
+        usernameStack.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 8).isActive = true
+        
+
+    }
+
+
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
