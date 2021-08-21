b47af9df995e600e5ec8779fdcdbbb94c1ee4c1e
Heang Ly <heanglypa@gmail.com>
2021-08-21 08:12:19 -0400

    image picker

--- InstagramFirestoreTut/InstagramFirestoreTut/Controllers/MainTabTableViewController.swift
+++ InstagramFirestoreTut/InstagramFirestoreTut/Controllers/MainTabTableViewController.swift
@@ -6,28 +6,31 @@
 //
 
 import UIKit
+import YPImagePicker
 
 class MainTabTableViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         configureViewController()
     }
 
     func configureViewController() {
         tabBar.tintColor = .black
+
+        self.delegate = self
+
         let layout = UICollectionViewFlowLayout()
         let feedVC = templateNavigationController(selectedImage: #imageLiteral(resourceName: "home_selected"), unselectedImage: #imageLiteral(resourceName: "home_unselected"), rootViewController: FeedViewController(collectionViewLayout: layout))
-        
+
         let searchVC = templateNavigationController(selectedImage: #imageLiteral(resourceName: "search_selected"), unselectedImage: #imageLiteral(resourceName: "search_unselected"), rootViewController: SearchViewController())
-        
+
         let imageSelectorVC = templateNavigationController(selectedImage: #imageLiteral(resourceName: "plus_unselected"), unselectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorViewController())
-        
-        let notificationVC = templateNavigationController(selectedImage: #imageLiteral(resourceName: "like_selected"), unselectedImage: #imageLiteral(resourceName: "like_unselected"), rootViewController:  NotificationViewController())
-        
+
+        let notificationVC = templateNavigationController(selectedImage: #imageLiteral(resourceName: "like_selected"), unselectedImage: #imageLiteral(resourceName: "like_unselected"), rootViewController: NotificationViewController())
+
         let profileVC = templateNavigationController(selectedImage: #imageLiteral(resourceName: "profile_selected"), unselectedImage: #imageLiteral(resourceName: "profile_unselected"), rootViewController: ProfileViewController(collectionViewLayout: layout))
-        
+
         setViewControllers([feedVC, searchVC, imageSelectorVC, notificationVC, profileVC], animated: false)
     }
 
@@ -38,4 +41,36 @@ class MainTabTableViewController: UITabBarController {
         return nav
     }
 
+    func didFinishPickingMedia(_ picker: YPImagePicker) {
+        picker.didFinishPicking { items, _ in
+            guard let selectedImage = items.singlePhoto?.image else { return }
+            print("SELECTED Image is \(selectedImage)")
+        }
+    }
+
+}
+
+
+extension MainTabTableViewController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        let index = viewControllers?.firstIndex(of: viewController)
+
+        if index == 2 {
+            var config = YPImagePickerConfiguration()
+            config.library.mediaType = .photo
+            config.shouldSaveNewPicturesToAlbum = false
+            config.startOnScreen = .library
+            config.screens = [.library]
+            config.hidesStatusBar =  false
+            config.hidesBottomBar = false
+            config.library.maxNumberOfItems = 1
+            
+            let picker = YPImagePicker(configuration: config)
+            picker.modalPresentationStyle = .fullScreen
+            present(picker, animated: true, completion: nil)
+            didFinishPickingMedia(picker)
+        }
+        
+        return true
+    }
 }
