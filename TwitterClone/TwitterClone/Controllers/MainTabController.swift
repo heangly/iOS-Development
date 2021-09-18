6fa1c80e12a1ce5491ee4c62a3ca85ebd669b9c1
Heang Ly <heanglypa@gmail.com>
2021-09-18 11:30:56 -0400

    set up

---
+++ TwitterClone/TwitterClone/Controllers/MainTabController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Heang Ly on 9/18/21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    //MARK: - Helpers
+    func configureUI(){
+        view.backgroundColor = .purple
+    }
+
+}
+
