736ed63cb8af19bdee29952ca5e788307e225819
Heang Ly <heanglypa@gmail.com>
2021-09-25 18:18:01 -0400

    set up

---
+++ SpotifyClone/SpotifyClone/Views/ViewControllers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by Heang Ly on 9/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureMainUI()
+    }
+
+    func configureMainUI(){
+        view.backgroundColor = .systemPink
+    }
+
+}
+
